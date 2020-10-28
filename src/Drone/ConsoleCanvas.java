2ba262bab2b0273e9757b2a5a4593893a0ba148e
Shavin Croos <at015244@student.reading.ac.uk>
2020-10-28 19:27:21 +0000

    Created showIt method and updated ConsoleCanvas method

--- src/Drone/ConsoleCanvas.java
+++ src/Drone/ConsoleCanvas.java
@@ -11,15 +11,21 @@ public class ConsoleCanvas {
         block = new char[x][y];
         for (int i = 0; i < blockX; i++){
             for (int j = 0; j < blockY; j++){
-                block[i][j] = ' ';
                 if (i == 0 || i == blockX - 1) {
                     block[i][j] = '#';
                 }
                 else if (j == 0 || j == blockY - 1){
                     block[i][j] = '#';
                 }
+                else {
+                    block[i][j] = ' ';
+                }
             }
         }
+    }
+
+    public void showIt(int dx, int dy, char ch) {
+        block[dx + 1][dy + 1] = ch;
     }
 
     public static void main(String[] args){
