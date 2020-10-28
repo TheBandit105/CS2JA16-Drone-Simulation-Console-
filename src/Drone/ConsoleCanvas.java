63d986dc88d25ba9b40f6dabe3ca12f1e2996fa7
Shavin Croos <at015244@student.reading.ac.uk>
2020-10-28 18:50:31 +0000

    Created ConsoleCanvas method

--- src/Drone/ConsoleCanvas.java
+++ src/Drone/ConsoleCanvas.java
@@ -1,6 +1,26 @@
 package Drone;
 
 public class ConsoleCanvas {
+    private int blockX;
+    private int blockY;
+    private char[][] block;
+
+    public ConsoleCanvas(int x, int y){
+        blockX = x;
+        blockY = y;
+        block = new char[x][y];
+        for (int i = 0; i < blockX; i++){
+            for (int j = 0; j < blockY; j++){
+                block[i][j] = ' ';
+                if (i == 0 || i == blockX - 1) {
+                    block[i][j] = '#';
+                }
+                else if (j == 0 || j == blockY - 1){
+                    block[i][j] = '#';
+                }
+            }
+        }
+    }
 
     public static void main(String[] args){
         ConsoleCanvas c = new ConsoleCanvas(10, 5);
