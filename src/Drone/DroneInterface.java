1182d6f63d53c42540a3a1fafa6cba0d804fd13c
Shavin Croos <at015244@student.reading.ac.uk>
2020-11-04 16:16:54 +0000

    Added animation (moving drones 10 times)

--- src/Drone/DroneInterface.java
+++ src/Drone/DroneInterface.java
@@ -1,6 +1,7 @@
 package Drone;
 
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class DroneInterface {
     private Scanner s;					// scanner used for input from user
@@ -11,9 +12,10 @@ public class DroneInterface {
         myArena = new DroneArena(10, 20);	    // create arena of size 20*6
 
         char ch = ' ';
+        System.out.println("\n ---------------- WELCOME TO THE DRONE SIMULATOR ------------------");
         do {
-            System.out.print("\nSelect one of the following options: " + "\n\tAdd drone - A " +  "\n\tGet info - I "
-                    + "\n\tDisplay arena - D " + "\n\tMove drones - M " + "\n\tExit - X " + "\n\n\t> ");
+            System.out.print("\nPlease select one of the following options: " + "\n\tAdd drone -> A " +  "\n\tGet info -> I "
+                    + "\n\tDisplay arena -> D " + "\n\tMove drones -> M " + "\n\tMove drones 10 times -> T" + "\n\tExit -> X " + "\n\n\t> ");
             ch = s.next().charAt(0);
             s.nextLine();
             switch (ch) {
@@ -37,6 +39,23 @@ public class DroneInterface {
                     System.out.println("\n");
                     myArena.moveAllDrones(myArena);
                     doDisplay();
+                    break;
+                case 't':
+                case 'T':
+                    if (myArena.numDrone.isEmpty() == false){
+                        for(int i = 0; i < 10; i++){
+                            myArena.moveAllDrones(myArena);
+                            doDisplay();
+                            System.out.println(myArena.toString());
+                            try {
+                                TimeUnit.MILLISECONDS.sleep(200); // Wait for 200ms
+                            } catch (InterruptedException e) {
+                                System.err.format("IOException: %s%n", e);
+                            }
+                        }
+                    } else if (myArena.numDrone.isEmpty() == true){
+                    System.out.println("Warning! Please insert drones to move!");
+                }
                     break;
             }
         } while (ch != 'X');						// test if end
